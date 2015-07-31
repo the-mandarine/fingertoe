e93afacd13c8f66c32bf4ed48ec728cc169699b9
Franklin Raccah <fraccah@arkena.com>
2015-07-31 11:37:17 +0200

    Upadted it to avoid security issues

--- fingertoed.py
+++ fingertoed.py
@@ -5,11 +5,13 @@ import sys
 import os
 from glob import glob
 
-HOST, PORT = "", 79
+HOST, PORT = "", 7979
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 
-PEOPLES = "people"
+PEOPLES = "/share/%s/finger/"
+DESC_FILE = "desc"
+PLAN_FILE = "plan"
 
 def init_log(root_log):
     ch = logging.StreamHandler(sys.stdout)
@@ -18,21 +20,33 @@ def init_log(root_log):
     ch.setFormatter(formatter)
     root_log.addHandler(ch)
 
+def get_file(fpath, all_paths, error=None):
+    answer = ""
+    if fpath in all_paths:
+        try:
+            with open(fpath, 'r') as people_file:
+                answer += people_file.read()
+        except:
+            pass
+    else:
+        if error is not None:
+            answer = error
+    return answer
+
 class FingerToeHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        LOG.info("{} {}".format(self.client_address[0], repr(self.data)))
-        self.answer = "No such user"
+        LOG.info("{} - {}".format(self.client_address[0], repr(self.data)))
+        self.answer = ""
         people = self.data.decode()
-        peoples_path = os.path.join(PEOPLES, '*')
-        people_path = os.path.join(PEOPLES, people)
+        peoples_path = os.path.join(PEOPLES % ('*'), '*')
+        desc_path = os.path.join(PEOPLES % (people), DESC_FILE)
+        plan_path = os.path.join(PEOPLES % (people), PLAN_FILE)
         all_peoples = glob(peoples_path)
-        if people_path in all_peoples:
-            try:
-                with open(people_path, 'r') as people_file:
-                    self.answer = people_file.read()
-            except:
-                pass
+        self.answer += get_file(desc_path, all_peoples, "No such user.")
+        self.answer += get_file(plan_path, all_peoples, "No Plan.")
+        if not people:
+            self.answer = "Must provide username"
         self.request.sendall(self.answer.encode())
 
 class FingerToServer(socketserver.TCPServer):
